8e900931e535d4ce9592df899e997f9d1477fd7a
turbo2121 <eyobaddis394@gmail.com>
2023-10-07 16:40:44 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('nav');
+ 
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
